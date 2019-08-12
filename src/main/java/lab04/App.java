f57c6a30e23c7e0e4aa07dd7c2585fba0660ccae
Jack Daniel Kinne <bravelemming@gmail.com>
2019-08-11 21:53:06 -0700

    added CL logic.

--- src/main/java/lab04/App.java
+++ src/main/java/lab04/App.java
@@ -8,34 +8,48 @@ import java.io.IOException;
 public class App {
 
     public static void main(String[] args) throws IOException {
-        //create bitmap and populate from path.
-        Bitmap bmp = new Bitmap("./src/main/resources/Coffee.bmp");
-        //make a black and white image
-        bmp.blackAndWhite();
-        bmp.writeOut("blackWhite");
-        //make a violet masterpiece
-        bmp.paintViolet();
-        bmp.writeOut("violet");
 
-//        if(args[2].equals( "violet") ){
-//            //do stuff
-//        }
-//        else if(args[2].equals("none")){
-//            //do nothing
-//        }
-//        else {
-//            System.out.println("please choose a valid transform:");
-//            System.out.println("1. violet:");
-//            System.out.println("2. black:");
-//        }
+        String path = "./src/main/resources/Coffee.bmp";
 
-        //int x = bmp2.getImage()
+        //run with IntelliJ
+        if (args.length < 1 ) {
+            //create bitmap and populate from path.
+            Bitmap bmp = new Bitmap(path);
+            //make a black and white image
+            bmp.blackAndWhite();
+            bmp.writeOut("blackWhite");
+            //make a violet masterpiece
+            bmp.paintViolet();
+            bmp.writeOut("violet");
+        }
+        else {
+            //run with command line args
+            path = args[0];
+
+            if(args[2].equals( "violet") ){
+                //create bitmap and populate from path.
+                Bitmap bmp = new Bitmap(path);
+                //make a violet masterpiece
+                bmp.paintViolet();
+                bmp.writeOut("violet");
+            }
+            else if(args[2].equals("black")){
+                //create bitmap and populate from path.
+                Bitmap bmp = new Bitmap(path);
+                //make a black and white image
+                bmp.blackAndWhite();
+                bmp.writeOut("blackWhite");
+            }
+            else {
+                System.out.println("please choose a valid transform:");
+                System.out.println("1. violet:");
+                System.out.println("2. black:");
+            }
+        }
     }
-
     //input from a bmp file
     //confirm we can see values in from the file input
     //create a class
     //create methods - transforms
     //output to a file
-
 }
